03e7d4aa1bfd2c3f722d1302be551641cde9b229
Ednaldo <martinsednaldo@outlook.com>
2018-09-01 01:24:36 -0300

    InsertionSort ( versão long [] )

--- src/model/InsertionSort.java
+++ src/model/InsertionSort.java
@@ -1,8 +1,6 @@
 
 package model;
 
-import java.util.List;
-
 /*******************************************************************************
  * @author marti                                                               *
  *  date: 21.07.2018                                                           *
@@ -13,26 +11,23 @@ public class InsertionSort implements AlgoritmoDeOrdenacao{
      *  Algoritmo           Complexidade                                       *
      *                  Melhor	Médio	Pior                                   *
      *  Insertion Sort	O(n)	O(n^2)	O(n^2)                                 *
-     * @param lista                                                            *
-     * @return lista ordenada                                                  *
+     *      @param list                                                        *
+     *      @return lista ordenada                                             *
      **************************************************************************/
     @Override
-    public List<Long> ordenar(List<Long> lista)
+    public long[] ordenar(long[] list)
     {
-        //busca enquanto nao chegar no fim da lista
-        int tamanhoLista = lista.size();
-        for(int i = 1; i < tamanhoLista; i++)
+        for(int i = 1; i < list.length; i++)
         {
             int j = i-1;
-            //pivo < anterior ?
-            if( lista.get(i) < lista.get(j) ) 
-            {
-                //colocar a verificacao do j>=0 antes, para nao dar NullPointer
-                for(;j>=0 && (lista.get(i) < lista.get(j)); j-- ){}
-                //retira o menor da posicao atual e adciona na posicao do pivo, empurrando o pivo pra frente.
-                lista.add(j+1, lista.remove(i));
-            } 
+            long pivo = list[i];    //guardar o pivo
+            //colocar a verificacao do j>=0 antes, para nao dar NullPointer
+            for(;j >= 0 && ( pivo < list[j] ); j-- )
+                list[j+1] = list[j];
+            //retira o menor da posicao atual e adciona na posicao do pivo, empurrando o pivo pra frente.
+            list[j+1] = pivo;
         }
-        return lista;
+        return list;
     }
+    
 }
