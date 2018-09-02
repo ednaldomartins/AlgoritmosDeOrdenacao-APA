cb819d6e146617d63839875c48aafa898b072066
Ednaldo <martinsednaldo@outlook.com>
2018-09-02 00:38:41 -0300

    SelectionSort ( long [] list )

--- src/model/SelectionSort.java
+++ src/model/SelectionSort.java
@@ -1,47 +1,38 @@
 
 package model;
 
-import java.util.List;
+import util.FuncoesLista;
 
-/**
- * @author marti
- *  date: 23.07.2018
- */
+/*******************************************************************************
+ * @author marti                                                               *
+ *  date: 23.07.2018                                                           *
+ ******************************************************************************/
 public class SelectionSort implements AlgoritmoDeOrdenacao{
  
     /***************************************************************************
-     *  Algoritmo           Complexidade                                       *
-     *                  Melhor	Médio	Pior                                   *
-     *  Selecion Sort	O(n^2)	O(n^2)	O(n^2)                                 *
-     * @param lista                                                            *
-     * @return lista ordenada                                                  *
+     *  Algoritmo:          Selecion Sort                                      *
+     *  Caso:               Melhor          Médio           Pior               *
+     *  Complexidade:       O(n^2)          O(n^2)          O(n^2)             *
+     *      @param list                                                        *
+     *      @return lista ordenada                                             *
      **************************************************************************/
     @Override
-    public List<Long> ordenar(List<Long> lista)
+    public long [] ordenar(long [] list)
     {
-        int tamanhoLista = lista.size();
-        for(int i = 0; i < tamanhoLista ; i++) 
-        {
-            //k (pivo)
-            int k = i;
-            for(int j = i+1; j < tamanhoLista; j++) 
-            {
-                if (lista.get(j) < lista.get(k))
-                {
-                    //novo pivo eh quem estah na posicao j atual.
-                    k = j;
-                }
+        for(int i = 0; i < list.length ; i++) 
+        {   //pivo inicial
+            int pivo = i;
+            for(int j = i+1; j < list.length; j++) 
+            {   //se TRUE entao, pivo agora eh quem estah na posicao j atual
+                if (list[j] < list[pivo])
+                    pivo = j;       
             }
-            
-            if (k != i)
-            {
-                //adicionando na nova posicao, quem estou removendo da anterior
-                lista.add(i, lista.remove(k));
-                lista.add(k, lista.remove(i+1));
-            }
+            //se TRUE entao, encontrou novo pivo menor que o inicial
+            if (pivo != i)
+                FuncoesLista.swap(list, i, pivo);
         }
         
-        return lista;
+        return list;
     }
     
 }
