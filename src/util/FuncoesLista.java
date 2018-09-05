a89a5ae3bc394b36094c199d30dde6df0b090150
Ednaldo <martinsednaldo@outlook.com>
2018-09-05 08:45:27 -0300

    FuncoesLista - metodo copiarLista implementado

--- src/util/FuncoesLista.java
+++ src/util/FuncoesLista.java
@@ -13,4 +13,15 @@ public class FuncoesLista {
         list[pivo] = list[i];
         list[i] = temp;
     }
+    
+    public static void copiarLista ( long [] original, long [] copia )
+    {
+        if(copia.length >= original.length)
+        {
+            for(int i = 0; i < original.length; i++)
+                copia[i] = original[i];
+        }
+        else
+            System.err.println("Nao foi possivel copiar o vetor original, pois o orginal eh maior do que a copia");
+    }
 }
