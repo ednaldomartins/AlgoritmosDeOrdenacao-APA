d425b14a12837f8e1c367dbde26d5b784db344bb
Ednaldo <martinsednaldo@outlook.com>
2018-09-05 08:45:48 -0300

    CountingSort (long [] list)

--- src/model/CountingSort.java
+++ src/model/CountingSort.java
@@ -1,7 +1,7 @@
 
 package model;
 
-import java.util.List;
+import util.FuncoesLista;
 
 /******************************************************************************
  * @author marti                                                              *
@@ -10,51 +10,43 @@ import java.util.List;
 public class CountingSort implements AlgoritmoDeOrdenacao{
 
     /***************************************************************************
-     *  Algoritmo                    Complexidade                              *             
-     *                  Melhor  	Médio           Pior                   *
-     *  Counting Sort	O(n + k)	O(n + k)	O(n + k)               *
-     * @param list                                                             *
-     * @return list ordenada                                                   *
+     *  Algoritmo:      CountingSort                                           *
+     *  Caso:           Melhor  	Médio           Pior                   *
+     *  Complexidade:   O(n + k)	O(n + k)	O(n + k)               * 
+     *      @param list                                                        *
+     *      @return list ordenada                                              *
      **************************************************************************/
     @Override
-    public List ordenar (List<Long> list) 
+    public long [] ordenar (long [] list) 
     {
-        int i, f = list.size();
+        int i, f = list.length;
         int menor = Integer.MAX_VALUE, maior = Integer.MIN_VALUE;
         //encontrando o maior e o menor numero do array
         for(i = 0; i < f; i++)
         {
-            if(list.get(i) < menor )
-                menor = list.get(i).intValue();
-            if(list.get(i) > maior)
-                maior = list.get(i).intValue();
+            if(list[i] < menor )
+                menor = (int) list[i];
+            if(list[i] > maior)
+                maior = (int) list[i];
         }
         //calculando amplitude do vetor, do positivo ao negativo
         int amplitudeNegativa = (menor < 0) ? Math.abs(menor) : 0;
         //vetor de contagem de ocorrencias
         int [] countList = new int [ maior + amplitudeNegativa + 1];
         for(i = 0; i < f; i++)
-        {
-            countList[ list.get(i).intValue() + amplitudeNegativa ]++;
-        }
+            countList[ (int) list[i] + amplitudeNegativa ]++;
         //somando valores de ocorrencias para determinar a posicao do valor no vetor
         //comeca a incrementar uma posicao a frente da primeira ocorrencia
         for(i = menor + amplitudeNegativa + 1; i <= maior + amplitudeNegativa; i++)
-        {   
             countList[i] = countList[i] + countList[i-1];
-        }
         //criando o vetor ordenado
         long [] vetorOrdenado = new long [f];
         for(i = f-1; i >= 0; --i)
-        {   // de tras pra frente
-            int novaPosicao = --countList[ list.get(i).intValue() + amplitudeNegativa ];
-            vetorOrdenado[novaPosicao] = list.get(i);
+        {   // de tras pra frente para manter algoritmo estavel ( 3 pode ser diferente de 3, quando se trata de Objetos)  
+            int novaPosicao = --countList[ (int) list[i] + amplitudeNegativa ];
+            vetorOrdenado[novaPosicao] = list[i];
         }
-        //atualizando a list
-        list.clear();
-        for(i = 0; i < f; i++)
-            list.add(i, vetorOrdenado[i]);
-        
+        FuncoesLista.copiarLista(vetorOrdenado, list);
         return list;
     }
     
